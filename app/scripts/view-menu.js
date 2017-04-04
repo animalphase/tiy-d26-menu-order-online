4e55df065cec3872b35874cdda6e65c8de89944b
charli elwonger <celwonger@gmail.com>
2017-04-03 21:46:04 -0500

    menu alignment stuff

--- app/scripts/view-menu.js
+++ app/scripts/view-menu.js
@@ -7,7 +7,7 @@ import menuItemView from './view-menu-item.js';
 
 export default function menuView(store) {
   let $viewContent = $(`<section class="page-wrapper view-menu">
-                          <header class="menu-header"><h1>Moonicorn Café</h1></header>
+                          <header class="menu-header"><h1>Moonside Café</h1></header>
                         </section>`);
 
   let $menuSection = $(`<section class="menu">
@@ -20,12 +20,9 @@ export default function menuView(store) {
 
 
   let menu = store.getState().menuItems;
-    console.log(menu);
     for (let category in menu) {
-      console.log(category);
       $menuSection.append(`<h3 class="food-category">${category}</h3>`);
       menu[category].forEach( (menuItem, i, array) => {
-        console.log(menuItem);
         $menuSection.append(menuItemView(menuItem));
       });
   }
